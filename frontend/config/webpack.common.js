216055cf12b49248661ef6c686a89b590ae5e42e
Benjamin Folkens <benjaminfolkens@gmail.com>
2020-12-20 01:34:29 -0500

    🎉 add initial project folder structure and files

---
+++ frontend/config/webpack.common.js
@@ -0,0 +1,77 @@
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
+
+const config = require('./config');
+
+const commonConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: config.srcDir,
+        // exclude: /node_modules/,
+        exclude: /@babel(?:\/|\\{1,2})runtime/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              // babelrc: true,
+              cacheDirectory: true,
+              // configFile: true,
+              cacheCompression: false,
+              sourceMaps: config.isProd,
+              inputSourceMap: config.isProd,
+              cacheIdentifier: getCacheIdentifier(
+                config.isProd ? 'production' : config.isDev && 'development',
+                ['babel-plugin-named-asset-import', 'react-dev-utils']
+              ),
+              compact: config.isProd,
+              presets: [
+                [require.resolve('@babel/preset-env'), { modules: false }],
+                require.resolve('@babel/preset-react'),
+              ],
+              plugins: [
+                require.resolve('@babel/plugin-syntax-dynamic-import'),
+                [
+                  require.resolve('@babel/plugin-proposal-class-properties'),
+                  { loose: true },
+                ],
+                [
+                  require.resolve('@babel/plugin-transform-runtime'),
+                  {
+                    absoluteRuntime: false,
+                    corejs: false,
+                    helpers: config.isDev, // TURN TO FALSE IN PROD
+                    regenerator: true,
+                    useESModules: true,
+                  },
+                ],
+                config.isDev && require.resolve('react-refresh/babel'),
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new WebpackManifestPlugin({
+      // publicPath: config.rootDir,
+    }),
+  ],
+  resolve: {
+    plugins: [PnpWebpackPlugin, new ModuleScopePlugin('src', 'package.json')],
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  resolveLoader: {
+    plugins: [PnpWebpackPlugin.moduleLoader(module)],
+  },
+};
+
+module.exports = commonConfig;
---
+++ frontend/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import kleur from 'kleur';
+
+import './app.css';
+
+console.log(kleur.bold().cyan('Readying Application!'));
+
+export default function App() {
+  return <></>;
+}
